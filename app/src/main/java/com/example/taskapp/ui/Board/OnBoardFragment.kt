1c02ac7462c0c84a4e20fb23a6d911f7df8a3b20
Tashpolotov <Aza662485@gmail.com>
2022-12-15 15:40:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/Board/OnBoardFragment.kt
@@ -0,0 +1,52 @@
+package com.example.taskapp.ui.Board
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.example.taskapp.R
+import com.example.taskapp.databinding.FragmentOnBoardBinding
+
+
+class OnBoardFragment : Fragment() {
+    lateinit var binding: FragmentOnBoardBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOnBoardBinding.inflate(LayoutInflater.from(context), container, false)
+
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = BoardAdapter(childFragmentManager, this:: onSkipClick, this:: onNextClick)
+        binding.vpBoard.adapter = adapter
+        var indikator = binding.vpBoard
+        indikator.adapter=adapter
+        var indicator = binding.indikatorPager
+        indicator.setViewPager(indikator)
+        indicator.createIndicators(3,0)
+        adapter.registerDataSetObserver(indicator.dataSetObserver)
+
+    }
+
+    private fun onSkipClick(){
+
+        binding.vpBoard.currentItem = 2
+    }
+
+    private fun onNextClick(){
+
+        binding.vpBoard.currentItem += 1
+    }
+}
